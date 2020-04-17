b48c93621ccf0e1f830f48fabedf3542747cf429
samcandy <samcandy841204@gmail.com>
2020-04-17 13:43:43 +0800

    Update click version 7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     py_modules=[],
     install_requires=[
-        'click',
+        'click==7.0',
         'pandas==0.24.2',
     ],
     entry_points={'console_scripts': [
